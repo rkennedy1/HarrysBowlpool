1962698319da87322e8bf3ede5c4fe4fa3b5cef3
Mike Kennedy <mskennedy67@gmail.com>
2023-09-26 19:00:37 -0700

    BP-61 working version

--- Server/src/routes/versionUtil.ts
+++ Server/src/routes/versionUtil.ts
@@ -1,16 +1,20 @@
 import express, { Request, Response } from 'express';
 import { connection } from '../helpers/db';
 
+interface version {
+  currentVersion: number;
+}
+
 export async function getCurrentVersion(year: String): Promise<number> {
   let query = `SELECT currentVersion FROM version    
                 WHERE year = ${year}`;
   let values = [year];
-  var currentVersion = 0;
+
   return new Promise<number>((resolve, reject) => {
-    connection.query(query, function (err: Error, returnVersion: number) {
+    connection.query(query, function (err: Error, returnVersion: version[]) {
       if (err) reject(err);
-      console.log('current version from util: ' + currentVersion);
-      resolve(currentVersion);
+      console.log('current version from util: ' + JSON.stringify(returnVersion));
+      resolve(returnVersion[0].currentVersion);
     });
   });
 }
