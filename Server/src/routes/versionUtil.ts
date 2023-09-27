fae02e324c3b52b5a3f27211edb5c67e6580ccad
Mike Kennedy <mskennedy67@gmail.com>
2023-09-26 18:14:37 -0700

    BP-61 update versionutil

--- Server/src/routes/versionUtil.ts
+++ Server/src/routes/versionUtil.ts
@@ -1,22 +1,15 @@
 import express, { Request, Response } from 'express';
 import { connection } from '../helpers/db';
 
-export interface Version {
-  currentVersion: Number;
-}
 
-export function getCurrentVersion(year: String) {
+export function getCurrentVersion(year: String): number {
   let query = `SELECT currentVersion FROM version    
                 WHERE year = ${year}`;
   let values = [year]
   var currentVersion = 0
   connection.query(query, function (err: Error, returnVersion: number) {
-    if err {
-      
-    }
-    currentVersion = returnVersion
+    if (err) throw console.error(err);
+     Promise.resolve(returnVersion)
+    console.log("current version from util: " + currentVersion)
   })
-  console.log("current version from util: " + currentVersion)
-  return currentVersion
-
 }
