55db938e4a3067850010dd8a83d1eda6410926b4
rkennedy1 <rskennedy99@gmail.com>
2023-10-11 20:32:22 -0700

    BP-69: simple versioning with local storage

--- Web/src/util/bowlpoolRepo.ts
+++ Web/src/util/bowlpoolRepo.ts
@@ -8,32 +8,65 @@ export class bowlpoolRepo {
       : process.env.REACT_APP_API_URL;
 
   getGameData(year: number) {
+    let localGameData = localStorage.getItem('gameData' + year.toString());
+    let localGameVersion = localStorage.getItem('gameVersion') ?? 0;
+    let gameDataURL = localGameVersion
+      ? `${this.url}/gameData/${year}?version=${localGameVersion}`
+      : `${this.url}/gameData/${year}`;
     return new Promise<BowlGame[]>((resolve, reject) => {
       axios
-        .get(`${this.url}/gameData/${year}`)
+        .get(gameDataURL)
         .then((resp) => {
-          let data = resp.data.bowlData;
-          let sortedData = data.sort((a: BowlGame, b: BowlGame) => {
-            return (
-              new Date(a.startTime).valueOf() - new Date(b.startTime).valueOf()
+          let version = resp.data.version;
+          let data: BowlGame[];
+          if (version > localGameVersion) {
+            data = resp.data.bowlData;
+            let sortedData = data.sort((a: BowlGame, b: BowlGame) => {
+              return (
+                new Date(a.startTime).valueOf() -
+                new Date(b.startTime).valueOf()
+              );
+            });
+            localStorage.setItem(
+              'gameData' + year.toString(),
+              JSON.stringify(sortedData)
             );
-          });
-          resolve(sortedData);
+            localStorage.setItem('gameVersion', version);
+            resolve(sortedData);
+          } else if (localGameData) {
+            resolve(JSON.parse(localGameData));
+          }
         })
         .catch((resp) => alert(resp));
     });
   }
 
   getPlayerData(year: number) {
+    let localPlayerData = localStorage.getItem('playerData' + year.toString());
+    let localPlayerVersion = localStorage.getItem('playerVersion') ?? 0;
+    let playerDataURL = localPlayerVersion
+      ? `${this.url}/playerData/${year}?version=${localPlayerVersion}`
+      : `${this.url}/playerData/${year}`;
     return new Promise<Player[]>((resolve, reject) => {
       axios
-        .get(`${this.url}/playerData/${year}`)
+        .get(playerDataURL)
         .then((resp) => {
-          let data = resp.data.players;
-          let sortedPlayers = data.sort(
-            (a: Player, b: Player) => b.points - a.points
-          );
-          resolve(sortedPlayers);
+          let version = resp.data.version;
+          let data: Player[];
+          if (version > localPlayerVersion) {
+            data = resp.data.players;
+            let sortedPlayers = data.sort(
+              (a: Player, b: Player) => b.points - a.points
+            );
+            localStorage.setItem(
+              'playerData' + year.toString(),
+              JSON.stringify(sortedPlayers)
+            );
+            localStorage.setItem('playerVersion', version);
+            resolve(sortedPlayers);
+          } else if (localPlayerData) {
+            resolve(JSON.parse(localPlayerData));
+          }
         })
         .catch((resp) => alert(resp));
     });
