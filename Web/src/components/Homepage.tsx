f15ea5fa1052a75ef5dc198760e4635a126e9395
rkennedy1 <rskennedy99@gmail.com>
2023-12-01 19:25:01 -0800

    BP-72: Settings modal

--- Web/src/components/Homepage.tsx
+++ Web/src/components/Homepage.tsx
@@ -5,6 +5,23 @@ import React from 'react';
 import { Grid, Switch, Tooltip } from '@mui/material';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import { BowlpoolTable } from '../components/table/BowlpoolTable';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { SettingsModal } from './SettingsModal';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4
+};
 
 export function Homepage() {
   const [checked, setChecked] = useState(() => {
@@ -13,29 +30,49 @@ export function Homepage() {
     return initialValue;
   });
 
+  const [openSettings, setOpenSettings] = React.useState(false);
+  const handleOpenSettings = () => setOpenSettings(true);
+  const handleCloseSettings = () => setOpenSettings(false);
+
   const switchHandler = (e: React.ChangeEvent<any>) => {
     setChecked(e.target.checked);
     localStorage.setItem('checked', JSON.stringify(e.target.checked));
   };
 
   return (
-    <Grid container>
-      <Grid item xs={6}>
-        <h3 className="m-2">Harry's Bowlpool</h3>
+    <>
+      <Grid container>
+        <Grid item xs={4}>
+          <h3 className="m-2">Harry's Bowlpool</h3>
+        </Grid>
+        <Grid item xs={4}>
+          <Tooltip title="Enable this slider to hide the bowl game name, date and each team's record">
+            <FormControlLabel
+              name="switchBowlGameDetails"
+              control={<Switch checked={checked} onChange={switchHandler} />}
+              label="Hide bowl game details"
+            />
+          </Tooltip>
+        </Grid>
+        <Grid item xs={4}>
+          <Tooltip title="Settings Modal">
+            <FormControlLabel
+              name="settingsModal"
+              control={<Button onClick={handleOpenSettings}>Settings</Button>}
+              label=""
+            />
+          </Tooltip>
+        </Grid>
+        <Grid item xs={12}>
+          <BowlpoolTable year={2022} hideBowlData={checked} />
+        </Grid>
+        {/* <BowlpoolTabs hideBowlData={checked} /> */}
       </Grid>
-      <Grid item xs={6}>
-        <Tooltip title="Enable this slider to hide the bowl game name, date and each team's record">
-          <FormControlLabel
-            name="switchBowlGameDetails"
-            control={<Switch checked={checked} onChange={switchHandler} />}
-            label="Hide bowl game details"
-          />
-        </Tooltip>
-      </Grid>
-      <Grid item xs={12}>
-        <BowlpoolTable year={2022} hideBowlData={checked} />
-      </Grid>
-      {/* <BowlpoolTabs hideBowlData={checked} /> */}
-    </Grid>
+      <SettingsModal
+        open={openSettings}
+        handleOpen={handleOpenSettings}
+        handleClose={handleCloseSettings}
+      />
+    </>
   );
 }
---
+++ Web/src/components/SettingsModal.tsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent
+} from '@mui/material';
+import { Player } from '../util/DataModels';
+import { bowlpoolRepo } from '../util/bowlpoolRepo';
+
+const bpr = new bowlpoolRepo();
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4
+};
+
+interface settingsModalProps {
+  open: boolean;
+  handleOpen: () => void;
+  handleClose: () => void;
+}
+
+export const SettingsModal: React.FC<settingsModalProps> = (props) => {
+  const [playerData, setPlayerData] = useState<Player[]>([]);
+  const [playerName, setPlayerName] = useState<string>(() => {
+    const saved = localStorage.getItem('playerName') || '';
+    return saved;
+  });
+  useEffect(() => {
+    bpr.getPlayerData(2022).then((data: Player[]) => setPlayerData(data));
+  }, [2022]);
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setPlayerName(event.target.value as string);
+    localStorage.setItem('playerName', JSON.stringify(event.target.value));
+  };
+
+  return (
+    <div>
+      <Button onClick={props.handleOpen}>Open modal</Button>
+      <Modal
+        open={props.open}
+        onClose={props.handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            sx={{ mb: 2 }}
+          >
+            Select a player to watch
+          </Typography>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Player Name</InputLabel>{' '}
+            <Select
+              value={playerName}
+              label="Player Name"
+              onChange={handleChange}
+              defaultValue={playerName}
+            >
+              {playerData !== undefined &&
+                playerData.map((p, i) => {
+                  return (
+                    <MenuItem value={p.name} key={i}>
+                      {p.name}
+                    </MenuItem>
+                  );
+                })}
+            </Select>{' '}
+          </FormControl>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Currently Selected Player: {playerName}
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
