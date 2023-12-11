ee22400347072ce0ec34ce0717b7d02565ab8ba8
rkennedy1 <rskennedy99@gmail.com>
2023-12-10 17:32:20 -0800

    BP-82: script to initilizate game data

---
+++ Utilities/initializeGameData.py
@@ -0,0 +1,94 @@
+import cfbdAPI
+import sys
+from mysqlConnection import getMyDB
+import mysql.connector
+
+# Establish the database connection
+mydb = getMyDB()
+mydb.reconnect()
+cursor = mydb.cursor()
+
+# Establish the CFB API connection
+API = cfbdAPI.api
+
+
+def retrieveGameData(year):
+    records = API.get_team_records(year=year)
+    games = API.get_games(year=int(year), season_type="postseason", division="fbs")
+    return {"games": games, "records": records}
+
+
+def getRecord(team, records):
+    record = list(filter(lambda x: x.team == team, records))
+    return str(record[0].total.wins) + "-" + str(record[0].total.losses)
+
+
+def querySQL(query, values):
+    print(query, values)
+    try:
+        cursor.execute(query, values)
+    except mysql.connector.Error as e:
+        print(e)
+        return False
+    return True
+
+
+def insertGame(game, year):
+    query = """INSERT INTO bowlGames (gameId, homeTeam, awayTeam, bowlName, year) VALUES (%s, %s, %s, %s, %s)"""
+    values = (game.id, game.home_id, game.away_id, game.notes, year)
+    return querySQL(query, values)
+
+
+def insertTeam(game, year, isHome, records):
+    query = """INSERT INTO bowlTeams (teamId, bowlId, year, name, isHome, line, record, conference) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+    values = ()
+    if isHome:
+        values = (
+            game.home_id,
+            game.id,
+            year,
+            game.home_team,
+            isHome,
+            0,
+            getRecord(game.home_team, records),
+            game.home_conference,
+        )
+    else:
+        values = (
+            game.away_id,
+            game.id,
+            year,
+            game.away_team,
+            isHome,
+            0,
+            getRecord(game.away_team, records),
+            game.away_conference,
+        )
+    return querySQL(query, values)
+
+
+def loadGames(gameData, year):
+    successfulImports = 0
+    for game in gameData["games"]:
+        if (
+            insertGame(game, year)
+            and insertTeam(game, year, True, gameData["records"])
+            and insertTeam(game, year, False, gameData["records"])
+        ):
+            successfulImports += 1
+    return successfulImports
+
+
+def main(args):
+    gameData = retrieveGameData(int(args[1]))
+    successfulImports = loadGames(gameData, int(args[1]))
+
+    # Close the database connection
+    cursor.close()
+    mydb.commit()
+    mydb.close()
+    print("Bowl Games Added: ", successfulImports)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
